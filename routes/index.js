e3bd3a156e2b35097b31e305fc4910b6c8df48e1
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-10-02 07:09:18 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(
+    'index', 
+    { 
+      title: 'Home', 
+    }
+  );
 });
 
 router.get('/about', function(req, res, next) {
@@ -12,6 +18,34 @@ router.get('/about', function(req, res, next) {
     name: 'Chantelle'
 
   });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render(
+    'projectservices', 
+    { 
+      title: 'Projects',
+    }
+  );
+});
+
+router.get('/services', function(req, res, next) {
+  res.render(
+    'projectservices', 
+    { 
+      title: 'Services',
+    }
+  );
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render(
+    'index', 
+    { 
+      title: 'Contact',
+      name: 'Chantelle' 
+    }
+  );
 });
 
 
