46e08ef095e3522d30140045761d47d7655ad287
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-10-02 06:59:19 -0400

    Users Commit

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.get('/chantelle', function(req, res, next) {
-  res.send('This is my user page. I am Chantelle.');
+  res.render(
+    'users', 
+    { 
+      title: 'Chantelle', 
+      name: 'Chantelle' 
+    }
+  );
 });
 
 module.exports = router;
