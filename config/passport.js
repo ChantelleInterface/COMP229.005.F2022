e6cdf8a81dae7e2d47f4bfb15a34f6b2c19adfe2
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-10-19 12:34:54 -0400

    edit

--- config/passport.js
+++ config/passport.js
@@ -8,16 +8,16 @@ const passport = require('passport');
 module.exports = function() {
     const User = require('../models/user');
     
-// When a user is authenticated, Passport will
-// save its id_property to the session.
+ // When a user is authenticated, Passport will
+ // save its id_property to the session.
     
     passport.serializeUser((user, done) => {
         done(null, user.id);
     });
 
-//  Later on, when the user object is needed, 
-//  Passport will use the _id property to retrieve
-//  the user from the database.
+ //  Later on, when the user object is needed, 
+ //  Passport will use the _id property to retrieve
+ //  the user object from the database.
     
     passport.deserializeUser((user, done) => {
         User.findOne( 
