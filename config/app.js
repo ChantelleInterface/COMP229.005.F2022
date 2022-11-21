2478b2ad2749ea05f38fdf0277f17a3e3c1eae37
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-11-21 03:04:36 -0500

    Update app.js

--- config/app.js
+++ config/app.js
@@ -1,49 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-let compress = require('compression');
-let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
-let passport = require('passport');
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let logger = require("morgan");
+let compress = require("compression");
+let bodyParser = require("body-parser");
+let methodOverride = require("method-override");
+let passport = require("passport");
 
-var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
-var inventoryRouter = require('../routes/inventory');
+let indexRouter = require("../routes/index");
+let usersRouter = require("../routes/users");
+let inventoryRouter = require("../routes/inventory");
 
-var app = express();
+let app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Sets up passport
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/inventory', inventoryRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404, "Endpoint not found."))
+app.use(function (req, res, next) {
+  next(createError(404, "Endpoint not found."));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   // res.render('error');
-  res.json(
-    {
-      success: false,
-      message: err.message
-    }
-  )
+  res.json({
+    success: false,
+    message: err.message,
+  });
 });
 
 module.exports = app;
