18b5246c8620b1fa7bf0a64c6661ce4ff80730f8
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-11-21 08:55:43 -0500

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,106 +1,99 @@
-let User = require('../models/user');
-let passport = require('passport');
+let User = require("../models/user");
+let passport = require("passport");
 
-let jwt = require('jsonwebtoken');
-let config = require('../config/config');
-
+let jwt = require("jsonwebtoken");
+let config = require("../config/config");
 
 function getErrorMessage(err) {
-    console.log("===> Error: " + err);
-    let message = '';
+  console.log("===> Error: " + err);
+  let message = "";
 
-    if (err.code) {
-
-        switch (err.code) {
-            case 11000:
-            case 11001:
-                message = 'Username already exists';
-                break;
-                default:
-                    message = 'Something went wrong';
-        }
-    } else {
-        for (var errName in err.errors) {
-            if (err.errors[errName].message) message = err.errors[errName].message;
-        }
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = "Username already exists";
+        break;
+      default:
+        message = "Something went wrong";
     }
-    
-    return message;
-};
+  } else {
+    for (let errName in err.errors) {
+      if (err.errors[errName].message) message = err.errors[errName].message;
+    }
+  }
 
-module.exports.signup = function(req, res, next) {
-    
-    console.log(req.body);
+  return message;
+}
 
-    let user = new User(req.body);
-    user.provider = 'local';
-    console.log(user);
+module.exports.signup = function (req, res, next) {
+  console.log(req.body);
 
-    user.save((err) => {
-        if (err) {
-        let message = getErrorMessage(err);
+  let user = new User(req.body);
+  user.provider = "local";
+  console.log(user);
 
-        return res.status(400).json(
-            {
-            success: false, 
-            message: message
-        });
+  user.save((err) => {
+    if (err) {
+      let message = getErrorMessage(err);
+
+      return res.status(400).json({
+        success: false,
+        message: message,
+      });
     }
     return res.json({
-            success: true,
-            message: 'User created successfully!'
-        });
+      success: true,
+      message: "User created successfully!",
     });
+  });
 };
 
-module.exports.signin = function(req, res, next){
-    passport.authenticate(
-        'login', 
-    async (err, user, info) => {
-        try {
-        if (err || !user) {
-            return res.status(400).json({ 
-                success: false, 
-                message: err || info.message
-            });
-        }
-    
-        req.login(
-            user,
-            { session: false }, // will not store session
-            async (error) => {
-                if (error) {
-                return next(error);
-            }
+module.exports.signin = function (req, res, next) {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        return res.status(400).json({
+          success: false,
+          message: err || info.message,
+        });
+      }
 
-              // Generating the JWT token.
-                const payload = 
-                {
-                    id: user._id,
-                    email: user.email 
-                };
-                const token = jwt.sign(
-                {
-                    payload: payload
-                }, 
-                config.SECRETKEY, 
-                {
-                    algorithm: 'HS512', // will ecrypt token
-                    expiresIn: "20min" // session will expire in 20 minutes
-                });
-                return res.json({
-                success: true,
-                token: token 
-                });
-            });
-        } catch (error) {
-    
-            console.log(error);
-            return res.status(400).json(
+      req.login(
+        user,
+        { session: false }, // will not store session
+        async (error) => {
+          if (error) {
+            return next(error);
+          }
+
+          // Generating the JWT token.
+          const payload = {
+            id: user._id,
+            email: user.email,
+          };
+          const token = jwt.sign(
             {
-                success: false,
-                message: getErrorMessage(error)
-            });
+              payload: payload,
+            },
+            config.SECRETKEY,
+            {
+              algorithm: "HS512", // will ecrypt token
+              expiresIn: "20min", // session will expire in 20 minutes
+            }
+          );
+          return res.json({
+            success: true,
+            token: token,
+          });
         }
-    })(req, res, next);
-}
+      );
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({
+        success: false,
+        message: getErrorMessage(error),
+      });
+    }
+  })(req, res, next);
+};
