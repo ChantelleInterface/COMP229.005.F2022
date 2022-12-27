a332f56adc000de9baa10614bcc0a5d010cc4e03
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-12-27 13:28:07 -0500

    protecting sensitive information .env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-// Do not expose your credentials in your code.
+require('dotenv').config();
 
 module.exports = {
-    "ATLASDB": "mongodb+srv://dbuser:ygquknsvMvWZyh2k@cluster005.nn2nri9.mongodb.net/products?retryWrites=true&w=majority",
+    "ATLASDB": process.env.ATLASDB,
     "LOCALDB": "mongodb://localhost:27017/dbapp",
-    "SECRETKEY": "LdKQ9MXQfEqJdgZQfxqp7AID4iZTXowUF3BiMyR4nPkSiBOWZ69mhR8Bfa1uGsdh"
+    "SECRETKEY": process.env.SECRETKEY
 }
